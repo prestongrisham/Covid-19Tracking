17f6636e9878528a4574d0f7bd739f4d72504b98
Preston Grisham <prestongrisham@gmail.com>
2020-12-26 08:41:31 -0500

    Initial Commit

---
+++ Covid-19Tracking/Covid_19TrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Covid_19TrackingApp.swift
+//  Covid-19Tracking
+//
+//  Created by Preston Grisham on 12/26/20.
+//
+
+import SwiftUI
+
+@main
+struct Covid_19TrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
